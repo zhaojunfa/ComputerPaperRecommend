8d2a9c05142b08f09f44e5e3fbfbdbb6e5bbb8e9
zhaojunfa <990695165@qq.com>
2016-08-22 14:54:06 +0800

    Initial Commit

---
+++ ComputerPaperRecommender/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ComputerPaperRecommender
+//
+//  Created by 赵俊法 on 16/8/22.
+//  Copyright © 2016年 Eight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ComputerPaperRecommender/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ComputerPaperRecommender
+//
+//  Created by 赵俊法 on 16/8/22.
+//  Copyright © 2016年 Eight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
